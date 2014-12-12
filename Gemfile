8206ebc1ec8d1a2882f5e94a561787b7505b8edd
Alex Texter <attexter@amazon.com>
2014-12-12 13:44:02 -0800

    Fetch champion kill events for a match

--- Gemfile
+++ Gemfile
@@ -38,4 +38,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
-gem 'ruby-lol'
+gem 'ruby-lol', :path => "../ruby-lol"
--- app/controllers/killgraph_controller.rb
+++ app/controllers/killgraph_controller.rb
@@ -1,19 +1,39 @@
 require 'lol'
 
 class KillgraphController < ApplicationController
+	CHAMPION_KILL = "CHAMPION_KILL"
+
 	def index
-		@whatever = "whatever"
 	end
 
 	def lookup
 		username = params[:username]
 
-		client = Lol::Client.new "", {region: "na"}
+		client = Lol::Client.new "bc9ba4d5-e50c-41f0-b2f5-d8ab0aeda1b7", {region: "na"}
 
-		id = client.summoner.by_name(username)[0].id
-		@data = client.game.recent(id)
+		summoner_id = client.summoner.by_name(username)[0].id
+		matches = client.match_history.get(summoner_id)['matches']
+		
+		match = matches[0]
+		match_data = client.match.get(match["matchId"], true)
 
-		# @data = client.game.get(game_id)
+		get_kill_events_from_match(match_data, summoner_id)
+	end
 
+	def get_kill_events_from_match(match_data, summoner_id)
+		champion_kills = []
+		frames = match_data["timeline"]["frames"]
+		frames.each do |frame|
+			if frame["events"].present?
+				frame["events"].each do |event|
+					if event["eventType"] == CHAMPION_KILL
+						champion_kills << event
+					end
+				end
+			end	
+		end
+
+		@data = match_data
+		# @data = champion_kills
 	end
 end
