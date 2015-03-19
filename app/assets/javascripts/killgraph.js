d9b95eda9086dfb37dc93f666824e3579101c3f0
echen <echen@goodreads.com>
2015-03-19 14:42:41 -0700

    Get tooltips working

--- app/assets/javascripts/killgraph.js
+++ app/assets/javascripts/killgraph.js
@@ -3,16 +3,12 @@ yScaling = (512/ (14980 - 120));
 
 $(document).ready(function() {
 	attachMatchHistoryClickHandlers();
-	showAllPlotPoints();
+	hideAllPlotPoints();
+	attachTooltipClickHandlers();
 });
 
 var plotKillsFromMatchHistory = function(matchId) {
-	var $coordinates;
-	if (typeof(matchId) === 'undefined') {
-		$coordinates = $(".coordinate");
-	} else {
-		$coordinates = $(".matchCoordinatesContainer" + matchId).find(".coordinate");
-	}
+	var $coordinates = $(".matchCoordinatesContainer" + matchId).find(".coordinate");
 
 	for (var i = 0; i < $coordinates.length; i++) {
 		var $coordinate = $($coordinates[i]);
@@ -25,62 +21,48 @@ var plotKillsFromMatchHistory = function(matchId) {
 };
 
 var attachMatchHistoryClickHandlers = function() {
-	$(".matchBox").hover(showMatchPlotPoints, showAllPlotPoints);
+	$(".matchBox").hover(showMatchPlotPoints, hideAllPlotPoints);
 };
 
 var showMatchPlotPoints = function(event) {
-	// hide all other match points
-	$(".coordinate").hide();
+	hideAllPlotPoints();
 
-	console.log($(event.target));
 	var matchId = $(event.target).closest(".matchBox").data("match");
-  console.log("moused into " + matchId);
   plotKillsFromMatchHistory(matchId);
 };
 
-var showAllPlotPoints = function() {
-	console.log("showing all plot points");
+var hideAllPlotPoints = function() {
 	$(".coordinate").hide();
-	// plotKillsFromMatchHistory();
 };
 
-// var addMatches = function(json) {
+var attachTooltipClickHandlers = function() {
+	$tiphover = $(".tiphoverContainer");
+	$tiphoverText = $tiphover.find(".tiphover");
+	$(".tip").mouseover(function(event) {
+		var $target = $(event.target);
+		// Todo: fix when .tip div has a child that isn't tip and becomes the target
+		if (!$target.hasClass("tip")) {
+			$target = $target.parents(".tip");
+		}
+		$tiphoverText.html($target.data("tip-text"));
 
-// 	// Set up tooltips
-// 	$tiphover = $(".tiphoverContainer");
-// 	$tiphoverText = $tiphover.find(".tiphover");
+		var yoffset;
+		if (($target.position().top - $(window).scrollTop()) < ($(window).height() / 2)) {
+			$(".topCaret").show();
+			$(".bottomCaret").hide();
+			yoffset = $target.height() - parseInt($(".bottomCaret").css("border-bottom-width"));
+		} else {
+			$(".topCaret").hide();
+			$(".bottomCaret").show();
+			yoffset = -$tiphover.height() + parseInt($(".topCaret").css("border-top-width"));
+		}
 
-// 	var html = json["matches_history"];
-// 	$(".match_history").append(html);
-// 	$(".match_history").append(html);
-// 	$(".match_history").append(html);
-// 	$(".match_history").append(html);
+		$tiphover.show();
+		$tiphover.css("top", $target.position().top + yoffset);
+		$tiphover.css("left", $target.position().left - ($tiphover.outerWidth() / 2) + ($target.outerWidth(true) / 2));
+	});
 
-// 	$(".tip").mouseover(function(event) {
-// 		var $target = $(event.target);
-// 		// Todo: fix when .tip div has a child that isn't tip and becomes the target
-// 		if (!$target.hasClass("tip")) {
-// 			$target = $target.parents(".tip");
-// 		}
-// 		$tiphoverText.html($target.data("tip-text"));
-
-// 		var yoffset;
-// 		if (($target.position().top - $(window).scrollTop()) < ($(window).height() / 2)) {
-// 			$(".topCaret").show();
-// 			$(".bottomCaret").hide();
-// 			yoffset = $target.height() - parseInt($(".bottomCaret").css("border-bottom-width"));
-// 		} else {
-// 			$(".topCaret").hide();
-// 			$(".bottomCaret").show();
-// 			yoffset = -$tiphover.height() + parseInt($(".topCaret").css("border-top-width"));
-// 		}
-
-// 		$tiphover.show();
-// 		$tiphover.css("top", $target.position().top + yoffset);
-// 		$tiphover.css("left", $target.position().left - ($tiphover.outerWidth() / 2) + ($target.outerWidth(true) / 2));
-// 	});
-
-// 	$(".tip").mouseout(function(event) {
-// 		$tiphover.hide();
-// 	});
-// };
+	$(".tip").mouseout(function(event) {
+		$tiphover.hide();
+	});
+};
